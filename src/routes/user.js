e8b2dd1878f48ecde239ea5bb0a0f1a098ddd440
TusB <tutran19112002@gmail.com>
2023-04-26 13:46:52 +0700

    finished flow login

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,15 +11,15 @@ router.get("/", verifyAccessToken, (req, res) => {
   });
 });
 
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const query = `select * from user where user_id=${+id};`;
-  connection.query(query, (err, results) => {
-    return res.status(200).json({
-      data: results,
-    });
-  });
-});
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const query = `select * from user where user_id=${+id};`;
+//   connection.query(query, (err, results) => {
+//     return res.status(200).json({
+//       data: results,
+//     });
+//   });
+// });
 
 router.put("/:id", verifyAccessToken, validate, (req, res) => {
   const { id } = req.params;
@@ -60,43 +60,43 @@ router.put("/:id", verifyAccessToken, validate, (req, res) => {
   );
 });
 
-router.post("/", validate, (req, res) => {
-  const { fullname, age, gender } = req.body;
-  const id = Math.floor(Math.random() * 100000000);
+// router.post("/", validate, (req, res) => {
+//   const { fullname, age, gender } = req.body;
+//   const id = Math.floor(Math.random() * 100000000);
 
-  connection.query(
-    `select * from user where user_id=${+id};`,
-    (err, result) => {
-      if (Object.keys(result).length === 0) {
-        connection.query(
-          "INSERT INTO user(user_id,fullname, gender, age) VALUES(?,?,?,?)",
-          [id, fullname, +gender, age],
-          (err, results) => {
-            if (err) {
-              console.log(err);
-              return;
-            }
+//   connection.query(
+//     `select * from user where user_id=${+id};`,
+//     (err, result) => {
+//       if (Object.keys(result).length === 0) {
+//         connection.query(
+//           "INSERT INTO user(user_id,fullname, gender, age) VALUES(?,?,?,?)",
+//           [id, fullname, +gender, age],
+//           (err, results) => {
+//             if (err) {
+//               console.log(err);
+//               return;
+//             }
 
-            return res.status(201).json({
-              message: "Created user",
-            });
-          }
-        );
-      } else {
-        return res.status(400).json({
-          message: "User exist",
-        });
-      }
-    }
-  );
-});
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
+//             return res.status(201).json({
+//               message: "Created user",
+//             });
+//           }
+//         );
+//       } else {
+//         return res.status(400).json({
+//           message: "User exist",
+//         });
+//       }
+//     }
+//   );
+// });
+// router.delete("/:id", (req, res) => {
+//   const { id } = req.params;
 
-  const query = `delete from user where user_id=?;`;
-  connection.query(query, [id], (err, results) => {
-    return res.status(200);
-  });
-});
+//   const query = `delete from user where user_id=?;`;
+//   connection.query(query, [id], (err, results) => {
+//     return res.status(200);
+//   });
+// });
 
 module.exports = router;
